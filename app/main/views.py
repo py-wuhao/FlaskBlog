1d59f3e823fa979fbba5ca2727e3533de6e5683d
wuhao <17762392194@163.com>
2019-01-04 14:22:06 +0800

    用户资料页面显示用户发表的文章

--- app/main/views.py
+++ app/main/views.py
@@ -20,27 +20,13 @@ def index():
         return redirect(url_for('.index'))
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', form=form, posts=posts)
-    # form = NameForm()
-    # if form.validate_on_submit():
-    #     user = User.query.filter_by(username=form.name.data).first()
-    #     if user is None:
-    #         user = User(username=form.name.data, role_id=3)
-    #         db.session.add(user)
-    #         db.session.commit()
-    #         session['known'] = False
-    #         # send_email('15392746632@qq.com','new user','mail/new_user',user=user)
-    #     else:
-    #         session['known'] = True
-    #     session['name'] = form.name.data
-    #     form.name.data = ''
-    #     return redirect(url_for('.index'))
-    # return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit_profile', methods=['GET', 'POST'])
