198bff482a662accd97b2b610fd1fe4af258d7de
wuhao <17762392194@163.com>
2019-01-12 08:51:22 +0800

    修改页面文字

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,19 +6,19 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1.64), Email()])
-    password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('keep me logged in')
-    submit = SubmitField('Log In')
+    email = StringField('邮箱', validators=[DataRequired(), Length(1.64), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('30天免登陆')
+    submit = SubmitField('登陆')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                                                                          'Usernames must have only letters, numbers, dots or underscores')])
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    password = PasswordField('password', validators=[DataRequired(), EqualTo('password2', message='密码不匹配')])
-    password2 = PasswordField('Confirm password', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    email = StringField('邮箱', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('密码', validators=[DataRequired(), EqualTo('password2', message='密码不匹配')])
+    password2 = PasswordField('确认密码', validators=[DataRequired()])
+    submit = SubmitField('注册')
 
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first():
